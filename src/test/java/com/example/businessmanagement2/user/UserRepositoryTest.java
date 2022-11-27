3052ac0a757ca9a3e3ab85b3073a3894d5ad3c2c
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-11-27 20:43:40 +0900

    DBテストできるように実装

--- src/test/java/com/example/businessmanagement2/user/UserRepositoryTest.java
+++ src/test/java/com/example/businessmanagement2/user/UserRepositoryTest.java
@@ -1,13 +1,15 @@
 package com.example.businessmanagement2.user;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
 
 import com.example.businessmanagement2.repository.user.UserEntity;
 import com.example.businessmanagement2.repository.user.UserRepository;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,4 +38,47 @@ public class UserRepositoryTest {
         );
   }
 
+  @Test
+  @DataSet(cleanBefore=true)
+  @Transactional
+  void ユーザーが登録いされてないときに空のリストが返ること(){
+    List<UserEntity> users = userRepository.findUserList();
+    assertThat(users).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  void 存在するユーザのIDを指定してユーザーが取得できること(){
+    UserEntity user = userRepository.findById(1).orElseThrow();
+    assertThat(user).isEqualTo(new UserEntity(1L,"○○○会社", "瀬川"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  public void ユーザーを新規登録できること(){
+    UserEntity ue = new UserEntity(null,"xxx会社", "瀬川3");
+    userRepository.create(ue);
+    List<UserEntity> actual = userRepository.findUserList();
+    assertThat(actual).hasSize(3);
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  public void キーに紐づく1件の更新が出来ること(){
+    userRepository.update(new UserEntity(1L, "xxx会社", "瀬川3"));
+    Optional<UserEntity> user = userRepository.findById(1L);
+    assertThat(user).hasValue(new UserEntity(1L, "xxx会社", "瀬川3"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  public void 指定したデーターを1件削除できること() {
+    userRepository.delete(1L);
+    List<UserEntity> actual = userRepository.findUserList();
+    assertThat(actual).hasSize(1);
+  }
 }
