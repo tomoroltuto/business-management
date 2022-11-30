0a97d08f7b2fb8316c0a4b558db30ce8764ba99b
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-11-30 21:37:12 +0900

    UserEntity.java内＠Dataから＠getterに変更

--- src/main/java/com/example/businessmanagement2/repository/user/UserEntity.java
+++ src/main/java/com/example/businessmanagement2/repository/user/UserEntity.java
@@ -1,9 +1,9 @@
 package com.example.businessmanagement2.repository.user;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class UserEntity {
 
